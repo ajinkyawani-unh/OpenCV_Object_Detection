543f0de65574a4cce8324a72865e03111178a6d6
ajinkyawani-unh <60481083+ajinkyawani-unh@users.noreply.github.com>
2020-07-15 15:10:19 -0400

    drawing objects only greater than area 1000 on separate blank image.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cv2
-
+import random
 
 img = cv2.imread("fuzzy.png", 1);
 cv2.imshow("Original", img)
@@ -8,9 +8,36 @@ cv2.imshow("Original", img)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY);
 blur = cv2.GaussianBlur(gray, (3,3), 0)
 
-cv2.imshow("Gray", gray);
-cv2.imshow("Blur", blur);
+# cv2.imshow("Gray", gray);
+# cv2.imshow("Blur", blur);
+
+thresh = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 205, 1)
+cv2.imshow("Binary", thresh);
+
+contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE);
+
+print(len(contours));
+
+filtered = [];
+
+for c in contours:
+    if cv2.contourArea(c) < 1000: continue
+    filtered.append(c)
+
+print(len(filtered))
+
+objects = np.zeros([img.shape[0], img.shape[1], 3], 'uint8')
+# cv2.imshow("Objects", objects )
+
+for c in filtered:
+    col = (random.randint(0, 255), random.randint(0, 255), random.randint(0,255));
+    cv2.drawContours(objects, [c], -1, col, -1)
+    area = cv2.contourArea(c);
+    p = cv2.arcLength(c, True);
+    print(area, p)
+
+cv2.imshow("Contour", objects)
 
 
 cv2.waitKey(0);
-cv2.destryAllWindows();
+cv2.destroyAllWindows();
